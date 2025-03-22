4db4f54c96f7a9b55e825be4ed57c3a6194cf696
danygonc <danyellerodrigues46@gmail.com>
2025-03-22 20:15:19 -0300

    Update xadreez.c

--- xadreez.c
+++ xadreez.c
@@ -1,4 +1,7 @@
+#include <stdio.h>
+
 int main(){
+  
     printf("\nMovimento da torre:\n");
 for (int i = 0; i < 5; i++){
 printf("direita\n");
@@ -19,6 +22,60 @@ while (i < 5){
         printf("esquerda\n");
         i++;
     } while (i<8);
+
+    printf("\nMovimento do cavalo:\n");
+int x = 0;
+for (int j = 0; j < 2; j++) {
+    printf("baixo\n");
+    x++;
+}
+
+int y = 0;
+while (y < 1){
+    printf("esquerda\n");
+    y++;
+
+}
+
+return 0;
+}#include <stdio.h>
+
+int main(){
+  
+    printf("\nMovimento da torre:\n");
+for (int i = 0; i < 5; i++){
+printf("direita\n");
+
+}
+
+printf("\nMovimento do bispo:\n");
+int i = 0;
+while (i < 5){
+    printf("cima, direita\n");
+    i++;
+}
+
+    printf("\nMovimento da rainha:\n");
+
+    i = 0;
+    do {
+        printf("esquerda\n");
+        i++;
+    } while (i<8);
+
+    printf("\nMovimento do cavalo:\n");
+int x = 0;
+for (int j = 0; j < 2; j++) {
+    printf("baixo\n");
+    x++;
+}
+
+int y = 0;
+while (y < 1){
+    printf("esquerda\n");
+    y++;
+
+}
 
 return 0;
 }
