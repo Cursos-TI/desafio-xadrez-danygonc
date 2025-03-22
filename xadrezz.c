92cc5b0519bb53278151560b2db4cabb6261327a
danygonc <danyellerodrigues46@gmail.com>
2025-03-22 17:54:08 +0000

    xadrez

---
+++ xadrezz.c
@@ -0,0 +1,24 @@
+int main(){
+    printf("\nMovimento da torre:\n");
+for (int i = 0; i < 5; i++){
+printf("direita\n");
+
+}
+
+printf("\nMovimento do bispo:\n");
+int i = 0;
+while (i < 5){
+    printf("cima, direita\n");
+    i++;
+}
+
+    printf("\nMovimento da rainha:\n");
+
+    i = 0;
+    do {
+        printf("esquerda\n");
+        i++;
+    } while (i<8);
+
+return 0;
+}
